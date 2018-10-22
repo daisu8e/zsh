c3538dea89ab5b239df37e9d62fccfc3f98dcaca
kat2n <katsumata.coffee.150yen@gmail.com>
2018-10-22 14:57:21 -0700

    initialization

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd `dirname $0`
+
+ln -s ${PWD}/.zlogin ${HOME}/.zlogin
+ln -s ${PWD}/.zlogout ${HOME}/.zlogout
+ln -s ${PWD}/.zprofile ${HOME}/.zprofile
+ln -s ${PWD}/.zshenv ${HOME}/.zshenv
+ln -s ${PWD}/.zshrc ${HOME}/.zshrc
+ln -s ${PWD}/.zsh ${HOME}/.zsh
